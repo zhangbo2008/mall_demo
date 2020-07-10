3f28d07d0231c48282c3cb9abee0a49471626038
zhangbo2008 <15122306087@163.com>
2020-07-10 17:58:47 +0800

    55

--- android/app/src/main/kotlin/com/example/flutterapp009/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterapp009/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutterapp212321
+package com.example.flutterapp009
 
 import io.flutter.embedding.android.FlutterActivity
 
--- android/app/src/main/kotlin/com/example/learn_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/learn_flutter/MainActivity.kt
@@ -1,25 +1,6 @@
-//package com.fluttermoney.www
-//
-//import androidx.annotation.NonNull;
-//import io.flutter.embedding.android.FlutterActivity
-//import io.flutter.embedding.engine.FlutterEngine
-//import io.flutter.plugins.GeneratedPluginRegistrant
-//
-//class MainActivity: FlutterActivity() {
-//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-//        GeneratedPluginRegistrant.registerWith(flutterEngine);
-//    }
-//}
-package com.fluttermoney.www
+package com.example.learn_flutter
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
 }
